eae7a58ecd744cf5f722bc8cdae4b8188b2e8887
okamu714 <ryohei714ok@gmail.com>
2024-11-16 23:59:20 +0900

    テスト

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="text-center bg-blue-500 text-white p-4 mt-4">
+    <div className="text-center bg-blue-500 size-6 text-white p-4 mt-4">
       <h1 className="text-3xl">Welcome to AnteKnowledge Journal</h1>
     </div>
   );
