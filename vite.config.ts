606d04dbcb41f7ccf0ded03c4f7dfe9e013f2d97
okamu714 <ryohei714ok@gmail.com>
2024-11-17 11:49:38 +0900

    viteの設定変更

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // プロジェクトのベースパスを設定
+  build: {
+      outDir: 'dist', // 出力先ディレクトリ
+  },
   plugins: [react()],
 })
