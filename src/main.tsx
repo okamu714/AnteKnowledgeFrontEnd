f7cd9f4a28858d0ac5723159a277ccdee510ff7a
okamu714 <ryohei714ok@gmail.com>
2024-12-01 20:42:46 +0900

    余計な部分を削除

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Amplify } from 'aws-amplify';
 import awsExports from './aws-exports';
-import { loadStripe } from '@stripe/stripe-js';
 import App from './App';
 import './index.css';
 
