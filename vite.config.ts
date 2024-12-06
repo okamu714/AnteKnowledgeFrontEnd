09485852a69d7f9a27812844815982b4b35b13fb
okamu714 <ryohei714ok@gmail.com>
2024-12-06 16:28:38 +0900

    一部変更

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: './', // プロジェクトのベースパスを設定
   build: {
-      outDir: 'dist', // 出力先ディレクトリ
+    outDir: 'dist', // 出力先ディレクトリ
   },
   plugins: [react()],
-})
+  optimizeDeps: {
+    exclude: ['chunk-Z6VMWTYC'], // 問題の依存関係を指定
+  },
+});
